a266cb3c1d3c365c947e287c4e08836ce4364601
devwithNoumanButt <devwithnoumanb20@gmail.com>
2025-04-13 21:04:47 +0500

    Initial commit

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useUser } from '@clerk/nextjs';
+
+export default function Dashboard() {
+
+  return (
+    <div>
+      Loading... Admin Dashboard
+    </div>
+  );
+}
